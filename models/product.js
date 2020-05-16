b09c9c651703c3bde5cf7dedd284b33414175afc
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-05-15 20:00:16 -0600

    creacion del schema product

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+            name: {
+                type: String,
+                require: true,
+            },
+            price: {
+                type: String,
+                require: true,
+            },
+            description: {
+                type: String,
+                require: true,
+            },
+            img: {
+                type: String,
+            }
+        }
+
+
+
+        const Product = mongoose.model('Product', productSchema);
+
+        module.exports = { Product }
