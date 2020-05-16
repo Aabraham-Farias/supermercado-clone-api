7e29c91d42abec79b78d1697166ed6552b6fe868
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-05-15 20:15:37 -0600

    add plugin timestap

--- models/product.js
+++ models/product.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = mongoose.Schema({
-            name: {
-                type: String,
-                require: true,
-            },
-            price: {
-                type: String,
-                require: true,
-            },
-            description: {
-                type: String,
-                require: true,
-            },
-            img: {
-                type: String,
-            }
-        }
+    name: {
+        type: String,
+        require: true,
+    },
+    price: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    img: {
+        type: String,
+    }
+}, { timestamps: true })
 
 
 
-        const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
-        module.exports = { Product }
+module.exports = { Product }
